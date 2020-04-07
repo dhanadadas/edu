158eb042f3f1ac4c28c6de24398704ab01179f92
Dhanadadas <79803011493@ya.ru>
2020-04-07 19:00:30 +0530

    task 2 commit

---
+++ task2/task.js
@@ -0,0 +1,13 @@
+// if (2*4 == 8){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+let num = 50;
+if (num < 49) {
+    console.log('Неверно')
+} else if (num > 100) {
+    console.log('Много')
+} else {
+    console.log('Верно')
+}
