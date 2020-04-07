babf46b921da6a8a019cf71fc8b19838f6787b2a
Dhanadadas <79803011493@ya.ru>
2020-04-07 15:50:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// 'use st'
+// dsad
+// sad
+//
+// var lestBolder = 1;
+// const lestBolderWidth = 1;
+// console.log(lestBolderWidth);
+// let second = 2;
+// const  pi = 3.14;
+// console.log(4/0);
+// console.log('string' * 9);
+// let something=22;
+// console.log(something);
+// let obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+// console.log(obj['name']);
+//
+// //array
+// let arr = ['plan.png','orange.png','apple.png'];
+// console.log(arr[1]);
+// alert("Hari!");
+// let qw=confirm("Are you from?");
+// alert(qw);
+// let qw=+prompt("Are you from?", "32");
+// console.log(typeof (qw));
+// console.log(typeof (null));
+// console.log('arr' + ' - object');
+// console.log(5 + ' - object');
+
+'use strict';
+
+
+let incr=10,
+    decr=10;
+console.log(5%2);
+let a=5;
+console.log(a);
